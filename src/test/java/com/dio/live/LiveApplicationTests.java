2f89e8037709d408a534de0a3d80ccad500c850e
lucasori <lucasorihashi11@gmail.com>
2021-08-15 00:59:41 -0300

    Rest-api

---
+++ src/test/java/com/dio/live/LiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.live;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
